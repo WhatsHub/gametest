49aba4bf0b910cda5e9234a707106999c9383ac0
Joschua <joschua.fink@gmx.de>
2017-02-08 17:04:39 +0100

    fixes the hidden snake outside the field (v2)

--- snakeMain.py
+++ snakeMain.py
@@ -1,4 +1,4 @@
-import pygame, sys, random
+import pygame, sys, random, time
 from pygame.locals import *
 from queue import *
 
@@ -24,6 +24,10 @@ NAVYBLUE    = ( 60,      60,    100)
 
 # draw the background on the surface object
 DISPLAYSURF.fill(NAVYBLUE)
+
+# sound mixer
+pygame.mixer.music.load("music/always.mp3")
+pygame.mixer.music.play(-1, 22.7)
 
 # snake x and y coordinates
 
@@ -65,6 +69,18 @@ def opposite(dir1, dir2):
     else:
         return False
 
+
+
+
+def gameover():
+    pygame.mixer.music.stop()
+    pygame.mixer.music.load("music/scratch.wav")
+    pygame.mixer.music.play()
+    time.sleep(2)
+    pygame.mixer.music.stop()
+    pygame.quit()
+    sys.exit()
+
 # main game loop
 while True:
 
@@ -82,11 +98,7 @@ while True:
     pygame.draw.rect(DISPLAYSURF, YELLOW, (headPosX - 5, headPosY - 5, 60, 60))
     pygame.draw.rect(DISPLAYSURF, GREEN, (headPosX, headPosY, 50, 50))
 
-    # what happens when the snake eats the apple
-    if headPosX - 5 == applePosX and headPosY - 5 == applePosY:
-        snake.append((snake[0][0], snake[0][1]))
-        applePosX = random.randint(0, 10) * 60
-        applePosY = random.randint(0, 7) * 60
+
 
     # draw the snake's tail
     for i in range(1, len(snake)):
@@ -112,30 +124,56 @@ while True:
         # moving the snake's head according to it's direction
         if direction == RIGHT:
             headPosX += 60
-            if headPosX >= 720:
+            if 717 <= headPosX >= 723:
                 headPosX = 5
 
         elif direction == DOWN:
             headPosY += 60
-            if headPosY >= 520:
+            if 517 <= headPosY >= 523:
                 headPosY = 5
 
         elif direction == LEFT:
             headPosX -= 60
-            if headPosX <= -55:
+            if -52 >= headPosX <= -58:
                 headPosX = 605
 
         elif direction == UP:
             headPosY -= 60
-            if headPosY <= -60:
+            if -57 >= headPosY <= -63:
                 headPosY = 425
+
+        # what happens when the snake eats the apple
+        if headPosX - 5 == applePosX and headPosY - 5 == applePosY:
+            snake.append((snake[0][0], snake[0][1]))
+            xyrows = [[True] * 8 for i in range(0, 11)]
+
+            for i in range(0, 11):
+                for j in range(0, 8):
+                    for k in range(0, len(snake)):
+                        snakeposx = snake[k][0]//60
+                        snakeposy = snake[k][1]//60
+                        if snakeposx <= 10 and snakeposy <= 7:
+                            if snakeposx == i and snakeposy == j:
+                                xyrows[i][j] = False
+
+            possiblespawns = [(0, 0)]
+            for i in range(0, 11):
+                for j in range(0, 8):
+                    if xyrows[i][j]:
+                        possiblespawns.append((i, j))
+
+            if len(possiblespawns) > 1:
+                possiblespawns.remove((0, 0))
+
+            random.shuffle(possiblespawns)
+            applePosX = possiblespawns[0][0] * 60
+            applePosY = possiblespawns[0][1] * 60
 
         # if the player hits one of the snakes tails the game is over
         for i in range(1, len(snake)):
             if snake[i][0] == headPosX and snake[i][1] == headPosY:
                 fpsClock.tick(1)
-                pygame.quit()
-                sys.exit()
+                gameover()
 
         snake[0] = (headPosX, headPosY)
 
@@ -146,30 +184,48 @@ while True:
     for event in pygame.event.get():
 
         if snakeoutside():
-            inputQueue.queue.clear()
-            inputQueue.put(direction)
+            if snake[0][0] > 655:
+                inputQueue.queue.clear()
+                inputQueue.put(RIGHT)
+            elif snake[0][0] < 5:
+                inputQueue.queue.clear()
+                inputQueue.put(LEFT)
+            elif snake[0][1] > 475:
+                inputQueue.queue.clear()
+                inputQueue.put(DOWN)
+            elif snake[0][1] < 5:
+                inputQueue.queue.clear()
+                inputQueue.put(UP)
+            else:
+                inputQueue.queue.clear()
+                inputQueue.put(direction)
 
         if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
+            pygame.mixer.music.stop()
             pygame.quit()
             sys.exit()
         elif event.type == KEYUP and event.key == K_RIGHT:
             if inputQueue.full():
                 inputQueue.queue.clear()
+                inputQueue.put(RIGHT)
             else:
                 inputQueue.put(RIGHT)
         elif event.type == KEYUP and event.key == K_LEFT:
             if inputQueue.full():
                 inputQueue.queue.clear()
+                inputQueue.put(LEFT)
             else:
                 inputQueue.put(LEFT)
         elif event.type == KEYUP and event.key == K_DOWN:
             if inputQueue.full():
                 inputQueue.queue.clear()
+                inputQueue.put(DOWN)
             else:
                 inputQueue.put(DOWN)
         elif event.type == KEYUP and event.key == K_UP:
             if inputQueue.full():
                 inputQueue.queue.clear()
+                inputQueue.put(UP)
             else:
                 inputQueue.put(UP)
 
